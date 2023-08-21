b96919279ce5583f37f490cec27967eb45f07ee2
Eros000 <e.ghilardi@hotmail.it>
2023-08-21 17:27:31 +0200

    form done

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +14,23 @@
     <title>Document</title>
 </head>
 <body>
+
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+
+    <form action="answer.php" method="get">
+
+        <textarea placeholder="phrase" name="phrase" id="" cols="30" rows="10">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Placeat cumque laborum fuga voluptatem quibusdam neque cum maiores totam laboriosam non corporis delectus unde ipsa voluptate, nisi reprehenderit quia, enim alias!</textarea>
+
+        <input type="text" name="censured-word"  placeholder="censured-word">
+
+        <input type="submit">
+
+    </form>
     
 </body>
 </html>
